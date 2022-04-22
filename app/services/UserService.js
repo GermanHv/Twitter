637e647490534b581e821ad077da0ea8d4b97ae0
GermanHv <ghove7@gmail.com>
2022-04-21 19:13:18 -0500

    Comentando/corrigiendo

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -5,16 +5,16 @@ class UserService {
         return new user(id,username,name, "bio vacia")
     }
     static getInfo(user){ //Se utiliza para mostrar los valores en lista los valores del objeto
-        return Object.values(user)
+        return [user.id, user.username,user.name,user="bio vacia"]
+        //    return Object.values(user)
     }
     static updateUserUsername(user, newUsername){//se actualiza el name con otro valor.
-     return user. = newUsername
+     return user.username= newUsername
     }
     static getAllUsernames ([user1,user2,user3]){
-        return [user1.username,user2.username,user3.username] 
-        //const usersUsernames = users.map( user => user.username)
-       //return usersUsernames
-        
+        return [user1.username,user2.username,user3.username]
+        //    const usersUsernames = users.map( user => user.username)
+        // return usersUsernames
     }
 }
 
