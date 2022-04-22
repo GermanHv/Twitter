4b84dbc28cfabaa9f76977a46add2d7b03830c40
GermanHv <ghove7@gmail.com>
2022-04-21 19:00:06 -0500

    3 y 4, requirimientos w/ tests

--- app/services/UserService.js
+++ app/services/UserService.js
@@ -1,11 +1,17 @@
 const user = require("./../models/user")
 
 class UserService {
-    static create(id, username,name){
+    static create(id, username,name){//Recibe solo los parametros de id, username y name
         return new user(id,username,name, "bio vacia")
     }
-    static getInfo(user){
+    static getInfo(user){ //Se utiliza para mostrar los valores en lista los valores del objeto
         return [user.id, user.username,user.name,user="bio vacia"]
+    }
+    static updateUserUsername(user, newUsername){//se actualiza el name con otro valor.
+     return user.username= newUsername
+    }
+    static getAllUsernames ([user1,user2,user3]){
+        return [user1.username,user2.username,user3.username]
     }
 }
 
--- test/models/UserService.test.js
+++ test/models/UserService.test.js
@@ -9,4 +9,28 @@ describe("Sprint 2:Tests for UserService", ()=>{
         expect(user.bio).toBeDefined()
     })
 
+    test("Requerimiento 2. Prueba de Unidad", ()=>{
+        const user = UserService.create(1,"GermanHv","German")
+        const userInfoInList = UserService.getInfo(user)
+        expect(userInfoInList[0]).toBe(1)
+        expect(userInfoInList[1]).toBe("GermanHv")
+        expect(userInfoInList[2]).toBe("German")
+        expect(userInfoInList[3]).toBe("bio vacia")
+    })
+
+    test("Requerimiento 3. Update username", ()=>{
+        const user2 = UserService.create(1,"GermanHv","German")
+        UserService.updateUserUsername(user2,"Vortex")
+        expect(user2.username).toBe("Vortex")
+    })
+
+    test("Requerimiento 4. Mostrar lista de usernames", ()=>{
+        const user1 = UserService.create(1,"GermanHv1", "German")
+        const user2 = UserService.create(2,"GermanHv2", "German")
+        const user3 = UserService.create(3,"GermanHv3", "German")
+        const usernames = UserService.getAllUsernames([user1,user2,user3])
+        expect(usernames).toContain("GermanHv1")
+        expect(usernames).toContain("GermanHv2")
+        expect(usernames).toContain("GermanHv3")
+    })
 })
